b45588f666ad8357030f4b065efd146c18bd0114
klmvsk <yowujeyi07@gmail.com>
2026-06-14 03:03:59 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ import json
 
 def load_people(filename):
     """
-    Читает людей из JSON файла и возвращает словарь, где ключ это имя.
+    Читает людей из json файла и возвращает словарь, где ключ это имя.
     """
     with open(filename, encoding="utf-8") as file:
         data = json.load(file)
